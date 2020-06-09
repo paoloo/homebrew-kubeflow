36278f2b3896917ffe8349cb46922cb3162ea470
Paolo Oliveira <paolocmo@gmail.com>
2020-06-09 17:45:36 -0300

    correct sha256

--- Formula/kfctl.rb
+++ Formula/kfctl.rb
@@ -3,7 +3,7 @@ class Kfctl < Formula
   homepage "https://www.kubeflow.org"
   url "https://github.com/kubeflow/kfctl/releases/download/v1.0.2/kfctl_v1.0.2-0-ga476281_darwin.tar.gz"
   version "1.0.2-0-ga476281"
-  sha256 "e0caad2a07148c9df7e384b145c28179"
+  sha256 "14133100d1b8e1c57bc04e2c99efd89697c867daf26113914b39b34aaf2df843"
   head "https://github.com/kubeflow/kfctl.git"
 
   bottle :unneeded
